b1c216d87bce4c84871e62bb5fa190a50913f074
lisali2023 <132962832+lisali2023@users.noreply.github.com>
2024-03-04 10:43:39 -0500

    20240304 Finished the Transfer data main function

--- Whole_Processing.py
+++ Whole_Processing.py
@@ -10,11 +10,11 @@ db_connection = connect_db()
 
 
 content = extract_wholetext_from_pdf(pdf_path)
-params = split_transfer_data(content)  # Ensure this function returns data in the correct order and format
+params_transfer = split_transfer_data(content)  # Ensure this function returns data in the correct order and format
 
 
-if params:
-    insert_data_to_db(db_connection, params)
+if params_transfer :
+    insert_data_to_db(db_connection, params_transfer )
     print("Data inserted successfully.")
 else:
     print("No data to insert.")
--- split_transfer_data.py
+++ split_transfer_data.py
@@ -1,6 +1,7 @@
 import datetime
 import re
 
+from Extract_wholetext_from_pdf import extract_wholetext_from_pdf
 
 
 def split_transfer_data(text):
@@ -9,9 +10,11 @@ def split_transfer_data(text):
         if "TRANSFER FROM AFFILIATED" in line and i + 1 < len(lines):
             data_row = lines[i + 1]
             # Regular expression to capture the structured data into seven groups
-            match = re.match(r"\((\d+)\) ([A-Z ]+) (\d+-[A-Za-z]+-\d+) (Transfer XFR) (\d+\.\d+) \((\$\d+\.\d+)\) \((\$\d+\.\d+)\)", data_row)
+            match = re.match(
+                r"\((\d+)\) ([A-Z ]+) (\d+-[A-Za-z]+-\d+) (Transfer XFR) (\d+\.\d+) \((\$\d+\.\d+)\) \((\$\d+\.\d+)\)",
+                data_row)
             if match:
-                ReportID = "IA_20210626_20210702_XIFENG"
+                ReportID = "IA_20210626_20210702_XIFENG_006"
                 BankNumber = match.group(1)
                 InsuredName = match.group(2)
                 TransactionDate = match.group(3)
@@ -20,7 +23,16 @@ def split_transfer_data(text):
                 AmountDue = match.group(6)
                 Balance = match.group(7)
                 TimeStamp = datetime.datetime.now()
-                return   ReportID, BankNumber, InsuredName, TransactionDate, TransactionType, CommissionPer, AmountDue, Balance, TimeStamp
+                return ReportID, BankNumber, InsuredName, TransactionDate, TransactionType, CommissionPer, AmountDue, Balance, TimeStamp
     return None
 
 
+pdf_path = r'D:\AIF(Lisa)\Projects\Accounting ETL from pdf\Jun 26 - July 2, 2021 - Mock.pdf'
+content = extract_wholetext_from_pdf(pdf_path)
+params = split_transfer_data(content)  # Ensure this function returns data in the correct order and format
+
+if params:
+    print(
+        f"BankNumber: {params[0]},BankNumber: {params[1]}, InsuredName: {params[2]}, TransactionDate: {params[3]}, TransactionType: {params[4]}, CommissionPer: {params[5]}, AmountDue: {params[6]}, Balance: {params[6],}, TimeStamp: {params[7],}")
+else:
+    print("Data not found or format is inconsistent.")
