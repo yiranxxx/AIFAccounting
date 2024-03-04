96d29d5353ab91b9708421c2c1caa1615b177357
Yiran <yiran.xiong@hotmail.com>
2024-03-04 16:55:03 -0500

    Extract Part 1 & Part 2 of the PDF

--- insert_data_to_db.py
+++
@@ -1,7 +0,0 @@
-
-def insert_data_to_db(connection, params):
-    cursor = connection.cursor()
-    sql_insert_query = """INSERT INTO Accounting_TransferFromAffiliated (ReportID,BankNumber, InsuredName, TransactionDate, TransactionType, CommissionPer, AmountDue, Balance,TimeStamp) VALUES (?, ?, ?, ?, ?, ?, ?,?,?)"""
-    cursor.execute(sql_insert_query, params)
-    connection.commit()
-    cursor.close()
