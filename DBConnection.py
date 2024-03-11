ae04424ea2c171f4bd695fd31b78ab1d2efdc762
lisali2024 <161749824+lisali2024@users.noreply.github.com>
2024-03-11 13:35:52 -0400

    Update DBConnection.py

--- DBConnection.py
+++ DBConnection.py
@@ -1,14 +1,1 @@
-import pyodbc
 
-
-def connect_db():
-    d = {
-        "server": "192.168.2.86,1433",
-        "database": "AIF_OLAP_Test_Intern",
-        "port": "1433",
-        "username": "OLAP_Intern",
-        "password": "1qaz@WSX"
-    }
-    connect_str = 'DRIVER={SQL Server};SERVER=' + d["server"] + ';DATABASE=' + d["database"] + ';UID=' + d[
-        "username"] + ';PWD=' + d["password"]
-    return pyodbc.connect(connect_str)
