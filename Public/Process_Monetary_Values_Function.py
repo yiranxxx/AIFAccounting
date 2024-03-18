cddb131ce2256288e2607b7b41f59a9a49cf6f9d
Yiran <yiran.xiong@hotmail.com>
2024-03-18 11:35:16 -0400

    0314

--- Public/Process_Monetary_Values_Function.py
+++
@@ -1,5 +0,0 @@
-def process_monetary_values(df, columns):
-    for col in columns:
-        # Remove currency symbol, commas, and parentheses for negative values
-        df[col] = df[col].replace({'\$': '', ',': '', '\(': '-', '\)': ''}, regex=True).astype(float)
-    return df
