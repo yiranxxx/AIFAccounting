8e267738bbb79ed2044e0965d26e76a90c28fbf4
lisali2023 <132962832+lisali2023@users.noreply.github.com>
2024-03-25 11:12:31 -0400

    0325 lisa test and change

--- Extract_PDF_Function.py
+++ Extract_PDF_Function.py
@@ -3,10 +3,6 @@ import os
 import pdfplumber
 import pandas as pd
 
-# directory_path = r"D:\AIF Intern\Accounting\test"
-# # Change the working directory to the specified path
-# os.chdir(directory_path)
-# file_name = "2.pdf"
 
 
 def Extract_PDF(file_name):
@@ -14,25 +10,25 @@ def Extract_PDF(file_name):
     df1 = None  # for report with multiple pages, details from page 3 to the page before last page
     df2 = None  # details of last page (if the report only has 3 pages, then it is the details of Page 3)
     # extract report header info
-    header_coordinates = ['0,610,770,500']
-    tables0 = camelot.read_pdf(file_name, flavor='stream', pages="3", table_areas=header_coordinates)
-    df0 = tables0[0].df
 
     # extract commission details
     with pdfplumber.open(file_name) as pdf:
         # Get the number of pages in the PDF
         last_page = len(pdf.pages)
-    page_range = '3-' + str(last_page - 1)
+
     table_coordinates = ['0,450,850,40']
-    min_row_num1 = 6  # minimum number of rows for multiple with only one record
-    min_row_num2 = 7  # minimum number of rows for single with only one record
+    min_row_num1 = 7  # minimum number of rows for multiple with only one record
+    min_row_num2 = 10  # minimum number of rows for single with only one record
 
     if int(last_page) > 3:
+        page_range = '3-' + str(last_page - 1)
+        header_coordinates = ['0,610,770,500']
+        tables0 = camelot.read_pdf(file_name, flavor='stream', pages="3", table_areas=header_coordinates)
+        df0 = tables0[0].df
 
         # extract contents from page 3 to the page before the last page
         tables1 = camelot.read_pdf(file_name, flavor='stream', pages=page_range,
                                    table_areas=table_coordinates, row_tol=10)
-
         for table in tables1:
             # Check the number of columns in the table
             num_columns = table.shape[1]
@@ -45,18 +41,19 @@ def Extract_PDF(file_name):
                 # Insert two empty columns after column 6
                 table.df.insert(loc=6, column=None, value='')
                 table.df.insert(loc=7, column=None, value='')
+
             elif num_columns == 10:
                 # Insert two empty columns after column 6
                 table.df.insert(loc=6, column=None, value='')
-
         # Initialize an empty list to store DataFrames
         dfs = []
-
         # Iterate over tables and append them to the list
         for table in tables1:
-            dfs.append(table.df)
+            df = table.df.reset_index(drop=True)
+            df.columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            dfs.append(df)
+            # Concatenate all DataFrames in the list into a single DataFrame
 
-        # Concatenate all DataFrames in the list into a single DataFrame
         df1 = pd.concat(dfs, ignore_index=True)
 
         # extract contents from the last page
@@ -66,32 +63,46 @@ def Extract_PDF(file_name):
 
         if num_rows1 <= min_row_num1:
             tables2 = camelot.read_pdf(file_name, flavor='stream', pages=str(last_page),
-                                       table_areas=table_coordinates, column_tol=-30, row_tol=10)
+                                       table_areas=table_coordinates, column_tol=-40, row_tol=10)
         else:
             tables2 = camelot.read_pdf(file_name, flavor='stream', pages=str(last_page),
                                        table_areas=table_coordinates, row_tol=10)
 
-        df2 = tables2[0].df
+        df2 = tables2[0].df.reset_index(drop=True)  # Reset index for the last page table
 
-    else:
+    elif int(last_page) == 3:
+        header_coordinates = ['0,610,770,500']
+        tables0 = camelot.read_pdf(file_name, flavor='stream', pages="3", table_areas=header_coordinates)
+        df0 = tables0[0].df
+
         last_page_test2 = camelot.read_pdf(file_name, flavor='stream', pages='3',
                                            table_areas=table_coordinates, row_tol=10)
         num_rows2 = last_page_test2[0].df.shape[0]
 
         if num_rows2 <= min_row_num2:
             tables2 = camelot.read_pdf(file_name, flavor='stream', pages='3',
-                                       table_areas=table_coordinates, column_tol=-30, row_tol=10)
+                                       table_areas=table_coordinates, column_tol=-40, row_tol=10)
         else:
             tables2 = camelot.read_pdf(file_name, flavor='stream', pages='3',
                                        table_areas=table_coordinates, row_tol=10)
-        df2 = tables2[0].df
+        df2 = tables2[0].df.reset_index(drop=True)  # Reset index for the last page table
+    elif int(last_page) < 3:
+        df0 = None
+        df1 = None
+        df2 = None
+
 
     return df0, df1, df2
 
-##test ###
-# from Extract_pdf_function import extract_pdf
-# _, df1, df2 = extract_pdf(file_name)
-# df2.to_csv('df1.csv', index=False, header=False)
+# # test
+# directory_path = r"D:\AIF Intern\Accounting\test"
+# # Change the working directory to the specified path
+# os.chdir(directory_path)
+# file_name = "Aug 21 - Aug 27, 2021.pdf"
+# _, df1, df2 = Extract_PDF(file_name)
+# df1.to_csv('test.csv', index=False, header=False)
+
+
 
 
 #  Assuming extract_pdf() function returns df0, df1, and df2
