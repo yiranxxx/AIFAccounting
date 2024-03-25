78fd379a9f593251d045a10a1e40dc3e3f1264bd
lisali2023 <132962832+lisali2023@users.noreply.github.com>
2024-03-25 11:02:24 -0400

    0325 lisa test and change

--- Clean_PaymentData_Function0322.py
+++ Clean_PaymentData_Function0322.py
@@ -142,7 +142,8 @@ file_name = r"\\AIF-NAS01\AIF_Interns\202312\Accounting\ErrorFile\0325\Aug 21 - 
 df0, df1, df2 = Extract_PDF(file_name)
 Info_df, CommissionID, End_Date_Year, AdvisorName, WeekNumber, StartDate, EndDate = Clean_Info(df0, "iA")
 PaymentData_df = Clean_Payment(df1, df2, CommissionID)
-Detail_df = Clean_Detail(df1, df2, CommissionID)
+# Detail_df = Clean_Detail(df1, df2, CommissionID)
 
 
 Insert_DB(Info_df,PaymentData_df)
+print("Success")
